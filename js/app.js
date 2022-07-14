361a1ae9a3e22f12e07f42e332a77eeca42d2b54
Emilse Arias <emilsearias@gmail.com>
2022-07-14 11:48:11 -0300

    se agrego la funcion verDetalle

--- js/app.js
+++ js/app.js
@@ -14,11 +14,18 @@ function crearColumna(serie){
             <img src="${serie.imagen}" class="card-img-top" alt="${serie.titulo}">
             <div class="card-body">
               <h5 class="card-title">${serie.titulo}</h5>
-              <a href="#" class="btn btn-primary">ver detalle</a>
+              <button class="btn btn-primary" onclick='verDetalle(${serie.codigo})'>ver detalle</button>
             </div>
           </div>
         </article>
     `
+}
+
+window.verDetalle = (codigo) =>{
+    console.log(codigo);
+    console.log(window.location)
+    console.log(window.location.origin+`/pages/detalle.html`)
+    window.location.href = window.location.origin+`/pages/detalle.html`
 }
 
 
